0121d5bfb796256dd166030b51898dfa62fd7be0
Prabhjot Singh <pjsingh1359@gmail.com>
2023-10-16 13:33:42 -0700

    add home page and router to add navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import HomePage from './pages/home.jsx'
+// import App from './App.jsx'
 import './index.css'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomePage />}>
+      {/* <Route path="dashboard" element={<Dashboard />} /> */}
+      {/* ... etc. */}
+    </Route>
+  )
+);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
