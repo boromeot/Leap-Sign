c91d4be3a4a67a03c437225a5a5217cfccd859d4
Prabhjot Singh <pjsingh1359@gmail.com>
2023-10-16 15:07:53 -0700

    add nav bar

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
 
 const HomePage = () => {
   return (
     <div>
+      <NavBar />
       <h1>Home Page</h1>
       <p>Welcome to the Home Page!</p>
     </div>
