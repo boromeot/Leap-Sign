f18eb7da9c9c96228fa92f55619994759cc9434e
Leslie Chou <chouyuchien@gmail.com>
2023-10-16 15:13:00 -0700

    added HomeTextVideo component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import HomeTextVideo from '../components/HomeTextVideo';
 
 const HomePage = () => {
   return (
+    <>
     <div>
       <h1>Home Page</h1>
       <p>Welcome to the Home Page!</p>
     </div>
+
+    <HomeTextVideo />
+    </>
   );
 };
 
