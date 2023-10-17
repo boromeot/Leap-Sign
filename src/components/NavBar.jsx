224d41aa2566bedcb87bc757b25ccf18246b8c02
Prabhjot Singh <pjsingh1359@gmail.com>
2023-10-17 11:18:17 -0700

    added logo and background color

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import "../styles/navbar.css";
+import frogImg from "../assets/frog.png";
 
 const NavBar = () => {
   return (
     <nav className='nav-bar'>
       <ul className='nav-bar__list'>
-        <li className='nav-bar__logo'><a href="/">Logo</a></li>
+        <li className='nav-bar__logo'><a href="/"><img className='frog-img' src={frogImg}/></a></li>
         <li className='nav-bar__app-name'>Catchy/app Name here</li>
         {/* conditional to check if user is signed in or not and render approprietly */}
         <li className='nav-bar__user-btns'>
