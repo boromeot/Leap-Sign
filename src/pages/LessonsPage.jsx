b98b37340d82092d391bbc51d7cac674299e40a2
Thenu <anbunachiappan@gmail.com>
2023-10-18 16:52:12 -0700

    worked on frog and lessons page

--- src/pages/LessonsPage.jsx
+++ src/pages/LessonsPage.jsx
@@ -1,11 +1,14 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import classes from '../styles/LessonsPage.module.css';
 import frogImg from '../assets/frog.png';
 import leaf from '../assets/lilypad.png';
+import SingleLesson from '../pages/SingleLesson'
 
 const shoot = () => {
-        alert("Great Shot!");
-    }
+    alert("Great Shot!");
+}
 
 const leafnode = () => {
     return (
@@ -23,35 +26,55 @@ const reset = () => {
     setUnlocked(false)
 };
 
+
 const FiveLeafNodes = () => {
+    const navigateTo = useNavigate();
 
     const [unlocked, setUnlocked] = useState(false);
-
-    if(!unlocked) {
+    function handleClick() {
+        navigateTo("/lessons/1");
+    }
+    if (!unlocked) {
         setUnlocked(true)
     }
 
     return (
         <div className={classes.leafclass}>
-                <div className={classes.lessonsLilyPad}>
-                    {!unlocked ? (
-                        leafnode()
-                    ):(
-                        <img className={classes.gray} src={leaf} alt="lilyPad" />
-                    )}
-                </div>
-                <div className={classes.lessonsLilyPad}>
-                    {leafnode()}
-                </div>
-                <div className={classes.lessonsLilyPad}>
-                    {leafnode()}
-                </div>
-                <div className={classes.lessonsLilyPad}>
-                    {leafnode()}
-                </div>
-                <div className={classes.lessonsLilyPad}>
-                    {leafnode()}
-                </div>
+
+            <div className={classes.frogContainer}  onClick={handleClick} >
+                <img className={classes.frogImg} src={frogImg} alt='FrogLogo' />
+                {leafnode()}
+               
+                {/* <button onClick={handleClick}> click me</button> */}
+            </div>
+            <div className={classes.lessonsLilyPad}>
+                {!unlocked ? (
+                    leafnode()
+                ) : (
+                    <img className={classes.gray} src={leaf} alt="lilyPad" />
+                )}
+            </div>
+            <div className={classes.lessonsLilyPad}>
+                {!unlocked ? (
+                    leafnode()
+                ) : (
+                    <img className={classes.gray} src={leaf} alt="lilyPad" />
+                )}
+            </div>
+            <div className={classes.lessonsLilyPad}>
+                {!unlocked ? (
+                    leafnode()
+                ) : (
+                    <img className={classes.gray} src={leaf} alt="lilyPad" />
+                )}
+            </div>
+            <div className={classes.lessonsLilyPad}>
+                {!unlocked ? (
+                    leafnode()
+                ) : (
+                    <img className={classes.gray} src={leaf} alt="lilyPad" />
+                )}
+            </div>
 
         </div>
     )
