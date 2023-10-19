f181e1e644e81cecf62f33f1e26e1f5e15e19ea0
Prabhjot Singh <pjsingh1359@gmail.com>
2023-10-19 13:38:08 -0700

    add api route folder

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const apiRouter = require('./api');
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
@@ -13,5 +13,7 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
