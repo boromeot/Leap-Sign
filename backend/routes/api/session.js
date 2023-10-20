483b60e0821feddb7aa0182010384af2b8ab01ee
Prabhjot Singh <pjsingh1359@gmail.com>
2023-10-20 11:48:14 -0700

    add session restore method

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -53,4 +53,22 @@ router.delete(
   }
 );
 
+// Restore session user
+router.get(
+  '/',
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+      return res.json({
+        user: safeUser
+      });
+    } else return res.json({ user: null });
+  }
+);
+
 module.exports = router;
