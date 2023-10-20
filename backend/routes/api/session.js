729d2aefe6ca7aa67ce230b1e0b753a0465f74a9
Prabhjot Singh <pjsingh1359@gmail.com>
2023-10-20 11:44:01 -0700

    add logout method

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -44,4 +44,13 @@ router.post(
   }
 );
 
+// Log out
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
 module.exports = router;
