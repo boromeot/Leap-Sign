b5dde1372d36c527b8d6b298cc4d7168b1e8a468
Leslie Chou <chouyuchien@gmail.com>
2023-10-24 14:52:33 -0700

    restyle navbar and added app name in the home/about pages

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -10,12 +10,12 @@ function Footer() {
         <footer>
             <div className={classes.footerContent}>
                 <div className={classes.footerImg}>
-                    <p>&copy; 2023 Copyright, appname</p>
+                    <p>&copy; 2023 Copyright, <span>LeapSign</span></p>
                     <img src={logo} alt={'appname'} className={classes.logoJump} />
                 </div>
                 <Link to='/about' className={classes.aboutLink}>
                     <p className={classes.info}>&#8505;</p>
-                    <p className={classes.aboutText}>About appname</p>
+                    <p className={classes.aboutText}>About <span>LeapSign</span></p>
                 </Link>
             </div>
         </footer>
