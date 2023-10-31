e97e0c25b4e0ff784b5d6c79a96f772288794ff2
Leslie Chou <chouyuchien@gmail.com>
2023-10-30 17:20:48 -0700

    working on ProfileButton component

---
+++ frontend/src/components/profileButton.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+
+function ProfileButton () {
+
+    const sessionUser = useState(state => state.sessionUser)
+    return (
+        <>
+        </>
+    )
+}
+
+
+export default ProfileButton;
