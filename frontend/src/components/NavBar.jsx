b97fdc15290ce7c0c3808ac9d86fc0b2d927c4b3
Leslie Chou <chouyuchien@gmail.com>
2023-10-30 17:50:55 -0700

    added user icon and created ProfileButton component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import ModalBackground from './ModalBackground';
+import ProfileButton from './profileButton';
 import frogImg from "../assets/frog.png";
+import userIcon from '../assets/user-icon.png';
 import * as sessionActions from "../store/session";
+
 import "../styles/navbar.css";
 
 const NavBar = () => {
@@ -11,6 +14,7 @@ const NavBar = () => {
   const userSession = useSelector(state => state.session.user);
   const [showSignIn, setShowSignIn] = useState(false);
   const [showSignUp, setShowSignUp] = useState(false);
+  const [showProfileButton, setShowProfileButton] = useState(false)
 
   function logout() {
     dispatch(sessionActions.logout());
@@ -34,6 +38,10 @@ const NavBar = () => {
     setShowSignUp(false);
   }
 
+  function showProfileButtonHandler () {
+    setShowProfileButton(!showProfileButton)
+  }
+
   return (
     <nav className='nav-bar'>
       <ul className='nav-bar__list'>
@@ -42,7 +50,14 @@ const NavBar = () => {
         <li className='nav-bar__user-btns'>
           {/* <Link to="/signin"><button>Sign In</button></Link>
           <Link to="/signup"><button>Sign Up</button></Link> */}
-          {userSession ? <button onClick={logout}>Log Out</button> : (
+          {userSession ? 
+          
+          <div className='userImage'>
+            <img src={userIcon} alt={userSession.username} onClick={showProfileButtonHandler} />
+            {showProfileButton ? <div><ProfileButton sessionUser={userSession} /></div> : null}
+          </div> 
+          
+          : (
             <div style={{display: 'flex', gap: '10px'}}>
               <button onClick={showSignInHandler}>Sign In</button>
               <button onClick={showSignUpHandler}>Sign Up</button>
--- frontend/src/components/profileButton.jsx
+++ frontend/src/components/profileButton.jsx
@@ -1,11 +1,28 @@
 import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom'
+import * as sessionActions from "../store/session";
 
 
-function ProfileButton () {
+function ProfileButton (props) {
 
-    const sessionUser = useState(state => state.sessionUser)
+    // const sessionUser = useState(state => state.sessionUser.user);
+    // console.log('session user in ProfileButton component: ', sessionUser);
+
+    const dispatch = useDispatch();
+
+    function logout() {
+        dispatch(sessionActions.logout());
+      }
+    
+
     return (
         <>
+        <div>
+            <p>{props.sessionUser.username}</p>
+            <Link to='/lessons'>Lessons</Link>
+            <button onClick={logout}>Log Out</button>
+        </div>
         </>
     )
 }
