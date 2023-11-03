63b950ec142eb51b4c5b17dce6208affcfab61d4
boromeot <boromeotyler@gmail.com>
2023-11-03 15:58:31 -0700

    added lessons data file

---
+++ frontend/src/utils/lessons.js
@@ -0,0 +1,24 @@
+export const lessons = {
+  1: {
+    'words' : ['thankyou', 'slow', 'again'],
+    'youtubeIds' : ['2W0BDFUjsG0', '6ky6oywprZ4', 'wTNv94AY-yE']
+  },
+  2: {
+    'words' : ['your', 'name', 'understand'],
+    'youtubeIds' : []
+  },
+  3: {
+    'words' : ['hello', 'goodbye', 'okay'],
+    'youtubeIds' : []
+  },
+  4: {
+    'words' : [],
+    'youtubeIds' : []
+  },
+  5: {
+    'words' : [],
+    'youtubeIds' : []
+  },
+}
+
+export default lessons;
