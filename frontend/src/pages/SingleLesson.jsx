425121be994fb6e61448d9d0f0814f29f2011fd4
boromeot <boromeotyler@gmail.com>
2023-11-03 15:58:12 -0700

    updated SingleLesson to play video and update the page when you succesfully sign the displayed word

--- frontend/src/pages/SingleLesson.jsx
+++ frontend/src/pages/SingleLesson.jsx
@@ -1,27 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import classes from '../styles/SingleLesson.module.css';
-import { useNavigate } from 'react-router-dom';
+import '../styles/SingleLesson.css';
+import { useNavigate, useParams } from 'react-router-dom';
 import FrogAnimation from './FrogAnimation';
 import Camera from '../components/Camera';
+import ReactPlayer from 'react-player';
+import lessons from '../utils/lessons';
 
 
 export default function lesson() {
   const navigate = useNavigate();
-  const lesson1 = ['thankyou', 'slow', 'again'];
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [currentWord, setCurrentWord] = useState(lesson1[currentIndex]);
+  
+  const { id } = useParams();
+  console.log(lessons[id].words)
+  const [currentWord, setCurrentWord] = useState(lessons[id].words[currentIndex]);
+  const [currentId, setCurrentId] = useState(lessons[id].youtubeIds[currentIndex]);
+  console.log(currentWord)
+
   let debounceTimeout;
 
   useEffect(() => {
-    setCurrentWord(lesson1[currentIndex]);
-  }, [currentIndex, currentWord])
+    setCurrentWord(lessons[id].words[currentIndex]);
+    setCurrentId(lessons[id].youtubeIds[currentIndex]);
+  }, [currentIndex]);
 
   function matchFunction() {
     if (!debounceTimeout) {
       debounceTimeout = setTimeout(() => {
-        setCurrentIndex((prev) => (prev + 1) % lesson1.length);
+        setCurrentIndex((prev) => (prev + 1) % lessons[id].words.length);
         debounceTimeout = null;
-      }, 2000); // Adjust the delay as needed (e.g., 1000ms = 1 second)
+      }, 1000); // Adjust the delay as needed (e.g., 1000ms = 1 second)
     }
   }
 
@@ -29,15 +38,20 @@ export default function lesson() {
     navigate('/lessons', { state: { animate: true, unlock: true } });
   }
 
+
   return (
-      <>
-        <div>
-          <Camera word={currentWord} threshold={0.9} matchFunction={matchFunction} />
-          <button onClick={navigateBackToLessons}>
-            Continue
-          </button>
-          <div>Please sign {currentWord}</div>
-        </div>
-      </>
+    <div className='singleLesson-cotainer'>
+      <div className='singleLesson-video'>        
+        <h1>Current sign : {currentWord}</h1>
+        <ReactPlayer 
+          url={`https://www.youtube.com/watch?v=${currentId}`}
+          playing={true}
+          muted={true}
+        />
+      </div>
+      <div className='singleLesson-camera'>
+        <Camera word={currentWord} threshold={0.9} matchFunction={matchFunction} />
+      </div>
+    </div>
   )
 }
