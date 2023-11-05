aff8145c6db9b9d6ba6660cdca1f9bea64a81b26
boromeot <boromeotyler@gmail.com>
2023-11-04 20:28:48 -0700

    removed camera component page and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,6 @@ import HomePage from './pages/home'
 import AboutPage from './pages/AboutPage'
 import LessonsPage from './pages/LessonsPage'
 import SingleLesson from './pages/SingleLesson'
-import Camera from './components/Camera';
 import NotFoundPage from './pages/404'
 
 import './App.css'
@@ -29,7 +28,6 @@ function App() {
         <Route exact={true} path="/" element={<HomePage />} />
         <Route exact={true} path="/about" element={<AboutPage />} />
         <Route exact={true} path='/lessons' element={<LessonsPage />}/>
-        <Route exact={true} path='/camera' element={<Camera />}/>
         <Route exact={true} path= '/lessons/:id' element={<SingleLesson />}/> 
         <Route path= '*' element={<NotFoundPage />} />
       </Routes>
